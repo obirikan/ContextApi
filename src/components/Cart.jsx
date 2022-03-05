04abbeca1c4164258cf168a7a2ed8d6bd04dea46
kelvin obiri <92574631+obirikan@users.noreply.github.com>
2022-03-05 19:48:13 +0000

    Update Cart.jsx
    + info

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+//To be able to use the context we export the created context in context.js
 import { useContext } from 'react';
 import { useEffect } from 'react';
+// I exported it directly so from here I can use the useContext() to use the created context
 import { Carts } from '../Context';
 import Single from './Single';
 
